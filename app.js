489e74c2b6abaf1481954215bdf11298dd68e522
Schefkin <alex.svek@gmail.com>
2025-02-04 23:46:01 +0200

    app upload

---
+++ app.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+
+// // modules
+const ejsMate = require('ejs-mate');
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+
+var SQLiteStore = require('connect-sqlite3')(session);
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
+
+
+
+
+
+
+
+const app = express()
+
+app.locals.pluralize = require('pluralize');
+
+
+// later part od env file or sth
+const port = 3000;
+
+// route stuff
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
+
+
+// ejs stuff
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// session stuff
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db' })
+}));
+
+app.use(passport.authenticate('session'));
+
+
+
+
+// routing
+app.use('/', indexRouter);
+app.use('/', authRouter);
+
+
+// if link isnt found
+app.all('*', (req, res, next) => {
+  res.send('404')
+})
+
+
+
+// port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,193 @@
+var express = require('express');
+const { body, validationResult } = require('express-validator');
+var db = require('../db.js');
+
+var router = express.Router();
+
+
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+
+
+// Cloudinary configuration
+cloudinary.config({
+    cloud_name: 'dd9trxmnc',
+    api_key: '894869558736479',
+    api_secret: 'xR8X-IFFpnFLyHh6xfMlFkj5QLA'
+});
+
+// Multer setup for handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 },  // 10MB limit
+}).single('image');
+
+
+
+// file check stuff
+const validColors = ['#3498db', '#2ecc71', '#f39c12', '#f1c40f', '#7f5539', '#95a5a6'];
+const allowedFormats = ['image/jpeg', 'image/jpg', 'image/png'];
+
+
+
+
+// form validation
+const formValidaton = [
+    // validate text field
+    body('text')
+        .exists()
+        .isString()
+        .isLength({ min: 1, max: 150 }).withMessage('Text field must not be empty nor over 150 characters long')
+        .trim() // Sanitize by trimming whitespace
+        .escape(),  // Sanitize by escaping HTML characters
+
+    // validate lnglat field
+    body('lnglat')
+        .exists(),
+
+    // color
+    body('color')
+        .exists()
+        .isIn(['#3498db', '#2ecc71', '#f39c12', '#f1c40f', '#7f5539', '#95a5a6'])
+];
+
+
+
+
+/* GET home page or map page based on login status */
+router.get('/', function (req, res, next) {
+    // Check if the user is logged in
+    if (!req.user) {
+        return res.render('home'); // Render 'home' if no user
+    }
+
+    // If the user is logged in, fetch the markers from the database
+    db.all('SELECT * FROM data', [], (err, rows) => {
+        if (err) {
+            console.error(err);
+            return res.render('map'); // Render 'map' even if there's an error in fetching data
+        } else {
+            // console.log(rows);
+            res.locals.filter = null; // Add any other necessary locals or filters
+            return res.render('map', { user: req.user, markers: rows }); // Render 'map' with user info and markers
+        }
+    });
+});
+
+
+// submitting form on map handling
+router.post('/map', upload, formValidaton, (req, res, next) => {
+    // error validation
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        // show error page
+        return res.render('error', { errors: errors.array() }); // Make sure this is a valid error handling page
+    }
+
+    const { text, lnglat, color } = req.body;
+    const file = req.file;
+    let imageUrl = null;
+
+    // checks before data can be uploaded to database
+    if (!text || typeof text !== 'string') {
+        return res.redirect('/');
+    }
+
+    if (!validColors.includes(color)) {
+        return res.redirect('/');
+    }
+
+    if (file && file.mimetype) {
+        if (!allowedFormats.includes(file.mimetype)) {
+            return res.redirect('/');
+        }
+    }
+
+    // lnglat checks 
+    try {
+        const tempLocation = JSON.parse(lnglat);
+        if (!tempLocation || typeof tempLocation.lat !== 'number' || typeof tempLocation.lng !== 'number') {
+            return res.redirect('/');
+        }
+
+        if (tempLocation.lat < -90 || tempLocation.lat > 90 || tempLocation.lng < -180 || tempLocation.lng > 180) {
+            return res.redirect('/');
+        }
+
+    } catch (error) {
+        return res.redirect('/');
+    }
+
+    // Proceed with file upload if file exists
+    if (file) {
+        cloudinary.uploader.upload_stream(
+            {
+                folder: 'MarkitMaps',
+                transformation: [
+                    { quality: 'auto', fetch_format: 'auto' },
+                    { width: 150, height: 150, crop: 'fill', gravity: 'auto' }
+                ]
+            },
+            (error, result) => {
+                if (error) {
+                    // console.error(error);
+                    return res.redirect('/');
+                }
+
+                imageUrl = result.secure_url; // Get Cloudinary URL
+                insertValuesIntoDatabase(imageUrl); // Continue with DB insert
+            }
+        ).end(file.buffer); // Upload the file buffer to Cloudinary
+    } else {
+        insertValuesIntoDatabase(imageUrl); // No file, insert with null imageUrl
+    }
+
+    // function that does the inserting, defined to avoid duplication
+    function insertValuesIntoDatabase(imageUrl) {
+        let today = new Date();
+        let dateString = today.toISOString().split('T')[0];  // This gives YYYY-MM-DD
+        const stmt = db.prepare("INSERT INTO data (lnglat, text, color, url, date) VALUES (?, ?, ?, ?, ?)");
+        stmt.run(lnglat, text, color, imageUrl, dateString, function (err) {
+            if (err) {
+                // console.error(err);
+                return res.redirect('/'); // Redirect in case of database error
+            }
+            return res.redirect('/'); // Successful insert, redirect to homepage
+        });
+    }
+});
+
+
+
+router.get('/info', function (req, res, next) {
+    res.render('info');
+});
+
+router.get('/contact', function (req, res, next) {
+    res.render('contact');
+});
+
+router.get('/error', function (req, res, next) {
+    res.render('error');
+});
+
+// // error handler middleware
+router.use((err, req, res, next) => {
+    // Log the error for debugging
+    if (err instanceof multer.MulterError) {
+        if (err.code === 'LIMIT_UNEXPECTED_FILE') {
+            // Handle the "Unexpected field" error specifically
+            res.redirect('/error');
+            next();
+        }
+    }
+    res.redirect('/error');
+    
+});
+
+
+
+module.exports = router;
